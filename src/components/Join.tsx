c2fc5760536b88b2657d82d16cf663ae1bb750e5
nhussaini <nasrullah_hussaini@yahoo.com>
2024-03-06 16:10:03 -0500

    create a form to join

--- src/components/Join.tsx
+++ src/components/Join.tsx
@@ -1,4 +1,14 @@
 const Join = () => {
-  return <div>Welcome to Join Component</div>;
+  const handleJoin = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+  return (
+    <form onSubmit={handleJoin}>
+      <input type="text" id="usernameInput" />
+      <button type="submit" id="joinBtn">
+        Join
+      </button>
+    </form>
+  );
 };
 export default Join;
