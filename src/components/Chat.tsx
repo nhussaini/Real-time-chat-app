6da2f3dc10e5fa9942f2cfbf10ed7f960f468c7d
nhussaini <nasrullah_hussaini@yahoo.com>
2024-03-07 20:42:20 -0500

    display the username

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,7 +1,14 @@
 import { useLocation } from 'react-router-dom';
 const Chat = () => {
   const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const username = queryParams.get('username');
+  //   const location = useLocation();
   console.log('Current URL from chat comp:', location.pathname);
-  return <div>Welcome to Chat component</div>;
+  return (
+    <div>
+      <h1 id="username">{username}</h1>
+    </div>
+  );
 };
 export default Chat;
