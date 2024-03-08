a375580cd8938d0e0c040a1d6ac8999e66d3c8c6
nhussaini <nasrullah_hussaini@yahoo.com>
2024-03-08 13:55:01 -0500

    send messages to all users

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,41 +1,132 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
+// import { useLocation } from 'react-router-dom';
+// // import WebSocket from 'ws';
+
+// const Chat = () => {
+//   const [userList, setUserList] = useState<string[]>([]);
+//   const [message, setMessage] = useState<string>('');
+//   //   const [username, setUsername] = useState<string>('');
+//   const location = useLocation();
+//   const queryParams = new URLSearchParams(location.search);
+
+//   const username = queryParams.get('username');
+//   const SERVER_ADDRESS = `ws://serve.skillreactor.io:8080/nasrullah?username=${username}`;
+//   let ws: WebSocket;
+//   useEffect(() => {
+//     ws = new WebSocket(SERVER_ADDRESS);
+//     ws.onopen = () => {
+//       console.log('Connected to WebSocket server!');
+//     };
+
+//     ws.onmessage = function (event) {
+//       const message = JSON.parse(event.data);
+//       console.log('data from websocket=>', message);
+//       if (message.type === 'users') {
+//         const userList = message.data.map((user: string) =>
+//           user.toLocaleLowerCase()
+//         );
+//         setUserList(userList);
+//         console.log('userList =>', userList);
+//       } else if (message.type === 'broadcast') {
+//         console.log('Received message==>', message.data);
+//       }
+//     };
+
+//     // Clean up WebSocket connection on unmount
+//     return () => {
+//       ws.close();
+//     };
+//   }, []);
+
+//   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+//     setMessage(event.target.value);
+//   };
+
+//   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+//     event.preventDefault();
+//     // Check if message is not empty
+//     if (message.trim() !== '') {
+//       //   const messageObject = { type: 'broadcast', data: message };
+//       console.log('message=>', message);
+//       const messageObject = { type: 'broadcast', data: message };
+//       if (ws) {
+//         ws.send(JSON.stringify(messageObject));
+//       }
+
+//       setMessage('');
+//     }
+//   };
+
+//   //   const location = useLocation();
+//   //   console.log('Current URL from chat comp:', location.pathname);
+//   return (
+//     <div>
+//       <h1 id="username">{username}</h1>
+//       <h1>Connected Users</h1>
+
+//       {userList.map((user, index) => (
+//         <p id={`user_${user}`} key={index}>
+//           {user}
+//         </p>
+//       ))}
+//       <form onSubmit={handleSubmit}>
+//         <input
+//           type="text"
+//           id="messageInput"
+//           value={message}
+//           onChange={handleInputChange}
+//         />
+//         <button id="messageSubmitBtn">Submit</button>
+//       </form>
+//     </div>
+//   );
+// };
+// export default Chat;
+
+import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
-// import WebSocket from 'ws';
 
 const Chat = () => {
   const [userList, setUserList] = useState<string[]>([]);
   const [message, setMessage] = useState<string>('');
-  //   const [username, setUsername] = useState<string>('');
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
-
   const username = queryParams.get('username');
+  const SERVER_ADDRESS = `ws://serve.skillreactor.io:8080/nasrullah?username=${username}`;
+  const [ws, setWs] = useState<WebSocket | null>(null);
 
   useEffect(() => {
-    const SERVER_ADDRESS = `ws://serve.skillreactor.io:8080/nasrullah?username=${username}`;
-    const ws = new WebSocket(SERVER_ADDRESS);
+    const socket = new WebSocket(SERVER_ADDRESS);
 
-    ws.onopen = () => {
+    socket.onopen = () => {
       console.log('Connected to WebSocket server!');
+      setWs(socket);
     };
 
-    ws.onmessage = function (event) {
+    socket.onmessage = (event) => {
       const message = JSON.parse(event.data);
-      console.log('data from websocket=>', message);
+      console.log('Data from WebSocket:', message);
       if (message.type === 'users') {
         const userList = message.data.map((user: string) =>
           user.toLocaleLowerCase()
         );
         setUserList(userList);
-        console.log('userList =>', userList);
+        console.log('UserList:', userList);
+      } else if (message.type === 'broadcast') {
+        console.log('Received message:', message.data);
       }
     };
 
     // Clean up WebSocket connection on unmount
     return () => {
-      ws.close();
+      if (
+        socket.readyState === WebSocket.OPEN ||
+        socket.readyState === WebSocket.CONNECTING
+      ) {
+        socket.close();
+      }
     };
-  }, []);
+  }, [SERVER_ADDRESS]);
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setMessage(event.target.value);
@@ -43,11 +134,14 @@ const Chat = () => {
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    // Code to send message...
+    // Check if message is not empty
+    if (message.trim() !== '' && ws && ws.readyState === WebSocket.OPEN) {
+      const messageObject = { type: 'broadcast', data: message };
+      ws.send(JSON.stringify(messageObject));
+      setMessage('');
+    }
   };
 
-  //   const location = useLocation();
-  console.log('Current URL from chat comp:', location.pathname);
   return (
     <div>
       <h1 id="username">{username}</h1>
@@ -70,4 +164,5 @@ const Chat = () => {
     </div>
   );
 };
+
 export default Chat;
